417e235f2e5f9ea9f85acd8d96785e3630a2cc8a
Jake Beresford <jake.beresford@health-union.com>
2019-08-06 16:17:05 -0400

    Add book form

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,20 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+    <hr>
+    <book-form :books="books" @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: "BookList",
   components: {
     BookItem,
+    BookForm
   },
   data() {
     return {
@@ -23,6 +27,14 @@ export default {
         {title: 'American Gods', author: 'Neil Gaiman'},
         {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
       ]
+    }
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({
+        title: bookTitle,
+        author: bookAuthor
+      })
     }
   }
 }
